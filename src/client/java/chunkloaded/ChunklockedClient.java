d07e9304d3de88b166187f70dd3632c711532872
JordanMartinWebDev <92476784+JordanMartinWebDev@users.noreply.github.com>
2026-01-10 15:43:55 -0600

    Initial commit

---
+++ src/client/java/chunkloaded/ChunklockedClient.java
@@ -0,0 +1,10 @@
+package chunkloaded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChunklockedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
